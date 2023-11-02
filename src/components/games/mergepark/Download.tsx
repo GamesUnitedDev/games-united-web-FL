ce2481f16bc1405dd3de38667d8ab977b6880050
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-11-02 23:42:17 +0300

    style: lg w

--- src/components/games/mergepark/Download.tsx
+++ src/components/games/mergepark/Download.tsx
@@ -7,7 +7,7 @@ function Download() {
   return (
     <section className="m-0 flex w-full items-center justify-center overflow-hidden p-0">
       <section className="flex w-full max-w-xl flex-wrap items-center justify-center gap-5 px-5 py-10 lg:max-w-theme lg:flex-nowrap">
-        <section className="flex w-full flex-col items-start justify-start gap-5 lg:min-w-[300px]">
+        <section className="flex w-full flex-col items-start justify-start gap-5 lg:max-w-[300px]">
           <h2 className="text-left text-2xl font-black text-black lg:text-4xl">
             Download Now!
           </h2>
@@ -30,7 +30,7 @@ function Download() {
         <img
           alt="Merge Park"
           src={PhoneIlsDesktop.src}
-          className="w-full translate-x-1/3 scale-150 object-contain object-center lg:-mr-0 lg:translate-x-0 lg:scale-125"
+          className="w-full translate-x-1/3 scale-150 object-contain object-center lg:-mr-0 lg:translate-x-0 lg:scale-100"
         />
         <section className="flex w-full flex-wrap items-center justify-center gap-5 lg:hidden">
           <GameDownloadButton
